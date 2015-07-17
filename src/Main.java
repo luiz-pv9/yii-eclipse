25958f6c22494b1146f12730d5980ef81cafd046
Luiz Paulo de Vasconcellos <luiz.pv9@gmail.com>
2015-07-17 02:01:08 -0300

    Simulando edição
    Adicionado terceira mensagem.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Segunda mensagem.");
+		System.out.println("Terceira mensagem.");
 	}
 }
